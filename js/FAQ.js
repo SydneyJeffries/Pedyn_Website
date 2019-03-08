5a88975d8e8bd4f98e4893c93f97194a3de211db
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-03-08 10:48:47 -0600

    update
    Deleted a bunch of useless files , added some JQ, and other random things

---
+++ js/FAQ.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+if(window.innerWidth > 900){
+  $('.slideTitle').animate({
+    position:'relative',
+    left: 0,
+    position:'initial',
+    paddingLeft:350,
+
+  },550);
+  }
+});
---
+++ js/aboutus.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+if (window.innerWidth > 900){
+$('.aboutusadress').animate({
+  position:'relative',
+  left: 0,
+  position:'initial',
+  paddingLeft:700,
+
+},950);
+}
+
+if(window.innerWidth > 900){
+  $('.slideTitle').animate({
+    position:'relative',
+    left: 0,
+    position:'initial',
+    paddingLeft:350,
+
+  },550);
+  }
+
+
+});
