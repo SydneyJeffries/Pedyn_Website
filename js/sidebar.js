71f124b98f08ca60686d402c88d73c0045ffd1ac
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-03-14 15:19:18 -0500

    Added Sidebar with some JS

---
+++ js/sidebar.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.offset().top <= scrollPos && refElement.offset().top + refElement.height() > scrollPos) {
+          $('#menu-center a').removeClass("active"); //added to remove active class from all a elements
+            currLink.addClass("active");
+            console.log(scrollPos);
+            console.log(refElement.offset().top);
+            
+            console.log(refElement.height());
+            console.log(refElement.offset().top + refElement.height() );
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
